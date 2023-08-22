337c79adbfd49faf274bef2dc0d378e219ea24f4
Mentabomber <mentae_mentology@libero.it>
2023-08-22 10:01:59 +0200

    route prefix comment added

--- routes/api.php
+++ routes/api.php
@@ -6,3 +6,5 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\ProjectController;
 
 Route::get('/Project-index' , [ProjectController::class, 'ProjectIndex']);
+
+// Route :: prefix('v1') -> group(function() {})
